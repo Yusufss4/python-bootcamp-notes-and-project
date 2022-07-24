fb357fdefe3d02144157ac0148d69ae57376729c
Yusufss <yusufsss4@gmail.com>
2022-07-24 23:31:55 +0300

    Added method for populating data

--- project-1-school-note-and-class-system/interface.py
+++ project-1-school-note-and-class-system/interface.py
@@ -6,6 +6,7 @@ def print_menu():
     print("2. Add Student to Course")
     print("3. Add Grade to Students")
     print("4. Calculate Grades for a Course")
+    print("5. Populate Example Data (Debug)")
 
 
 def add_course_menu(course_list):
@@ -50,7 +51,7 @@ def add_grades_to_students_menu():
         student = student_list.search_student_by_name(student_name, student_surname)
 
         student_grade = input("Enter the grade of the student: ")
-        course_name.add_grade_to_student(student,student_grade)
+        course_found.add_grade_to_student(student,student_grade)
         option_for_more_students = input("Dou want to continue: Y/N ")
 
         if option_for_more_students == "N":
@@ -61,6 +62,47 @@ def add_grades_to_students_menu():
 
 
 def calculate_grades_for_course():
+    course_name = input("Enter the name of the course: ")
+    course_found = course_list.search_course_by_name(course_name)
+
+    course_found.calculate_grade(calculation_type="manual")
+    course_found.export_excel_file()
+    course_found.describe()
+
+
+def populate_example_data():
+
+    s1 = rg.Student("Yusuf", "Savas")
+    s2 = rg.Student("Mehmet", "Yılmaz")
+    s3 = rg.Student("Ayşe", "Demir")
+    s4 = rg.Student("John", "Rave", 101) # Custom ID for a student.
+
+    course1 = rg.Lesson("Math", max_students=30) # Creating a lesson.
+    course1.add_student_to_lesson(s1)
+    course1.add_student_to_lesson(s2)
+    course1.add_student_to_lesson(s3)
+
+
+    course1.add_grade_to_student(s1, 60)
+    course1.add_grade_to_student(s2, 20)
+    course1.add_grade_to_student(s3, 50)
+
+    course1.CalculationGrades.return_current_grades()
+    course1.calculate_grade("manual")
+
+    course2 = rg.Lesson("Art", max_students=5)
+    course2.add_student_to_lesson(s3)
+    course2.add_student_to_lesson(s4)
+
+    course2.add_grade_to_student(s3, 10)
+    course2.add_grade_to_student(s4, 50)
+    course2.calculate_grade("manual")
+
+    course1.describe()
+    course2.describe()
+
+    course1.export_excel_file()
+    course2.export_excel_file()
     pass
 
 
@@ -81,6 +123,9 @@ if __name__ == '__main__':
             add_grades_to_students_menu()
         elif input_menu == "4":
             calculate_grades_for_course()
+        elif input_menu == "5":
+            populate_example_data()
+
 
 """
         s1 = rg.Student("Yusuf", "Savas")
